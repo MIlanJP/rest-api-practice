a5bea85522272ed202f6eee5ec6dd59b2e84c695
kyoolbaba <kyoolbaba@gmail.com>
2020-07-01 21:09:16 +0530

    Added CRUD Operations to the controller

--- src/main/java/com/restexample/controller/UserController.java
+++ src/main/java/com/restexample/controller/UserController.java
@@ -1,4 +1,29 @@
 package com.restexample.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("user")
 public class UserController {
+
+    @GetMapping
+    public String getUser(){
+        return "get user";
+    }
+
+    @PostMapping
+    public String createUser(){
+        return "createUser";
+    }
+
+    @PutMapping
+    public String updateUser(){
+        return "updateUser";
+    }
+
+    @DeleteMapping
+    public String deleteUser(){
+        return "deleteUser";
+    }
+
 }
